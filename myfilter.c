589ceccf5efd53cce63e148fbc2e1e97b32951d0
Noella James <noellajames@gmail.com>
2018-03-28 01:44:19 -0500

    forgot myfilter

--- myfilter.c
+++ myfilter.c
@@ -18,10 +18,6 @@ static char buffer[MAX_BUF_SIZE];
 
 static int binary_file = 0;
 
-
-int can_transition(unsigned char, unsigned int, int count);
-
-
 /* Filters input and prints out lines that matches the pattern . 
 For binary file a string indicating pattern was matched is printed.*/
 int filter_input(void) {
@@ -79,48 +75,4 @@ int filter_input(void) {
     }
 
     return total_matched;
-}
-
-
-
-
-/* Return true if the state engine can transition from the current state
-to the next state based on the input character. Else it returns false.
-count is to determine the start of the line */
-int can_transition(unsigned char uch, unsigned int in_index, int count) {
-    int index = in_index;
-    if (index < 0){
-        return 1;
-    }
-    unsigned size = my_states[index].size;
-    if (my_states[index].type == TYPE_START) {
-        if (count != 1) {
-            return 0;
-        }
-    }
-    if ((my_states[index].type == TYPE_REGULAR) || (my_states[index].type == TYPE_SPECIAL)
-        || (my_states[index].type == TYPE_START) || (my_states[index].type == TYPE_END)) {
-        for (unsigned k = 0; k < size; k++){
-            if ((my_states[index].ch[k] == MATCH_ALL_CHAR) && (my_states[index].type != TYPE_SPECIAL)){
-                return 1;
-            }
-
-            if (my_states[index].ch[k] == uch){
-                return 1;
-            }
-        }
-        return 0;
-    } else if (my_states[index].type == TYPE_INVERTED) {
-        for (unsigned k = 0; k < size; k++){
-            if (my_states[index].ch[k] == MATCH_ALL_CHAR) {
-                return 0;
-            }
-
-            if (my_states[index].ch[k] == uch){
-                return 0;
-            }
-        }
-        return 1;
-    }
-    return 0;
 }
